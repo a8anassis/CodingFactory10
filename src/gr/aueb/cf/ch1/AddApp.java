74f4c8da12083bc68086252a31933f250550cc6f
a8anassis <a8anassis@gmail.com>
2026-03-31 19:52:08 +0300

    chore(ch1): add AddApp class with addition logic for adding two integers and printing

---
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Adds two integers and prints
+ * the result to the console.
+ */
+public class AddApp {
+
+    public static void main(String[] args) {
+
+        // Δήλωση και αρχικοποίηση μεταβλητών
+        int num1 = 5;
+        int num2 = 12;
+        int result = 0;
+
+        // Λογική - Εντολές
+        result = num1 + num2;
+
+        // Εκτύπωση αποτελέσματος
+        System.out.println("Το αποτέλεσμα είναι: " + result);
+    }
+}
