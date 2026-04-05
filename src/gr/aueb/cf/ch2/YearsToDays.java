11ebdfe023aaf2106ee4e04236ac6694d65cc549
a8anassis <a8anassis@gmail.com>
2026-04-05 13:44:41 +0300

    chore(ch2): add Years to Days app

---
+++ src/gr/aueb/cf/ch2/YearsToDays.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.ch2;
+
+import java.util.Scanner;
+
+/**
+ * Ο χρήστης εισάγει την ηλικία του σε έτη (έστω ακέραιος)
+ * και το πρόγραμμα επιστρέφει την ηλικία σε ημέρες, όπου
+ * θεωρούμε ότι 1 έτος = 365 ημέρες. Για παράδειγμα, αν δοθεί
+ * ηλικία 2ο ετών, το αποτέλεσμα θα είναι 7300 ημέρες.
+ */
+public class YearsToDays {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputAgeInYears = 0;
+        int ageInDays = 0;
+        final int DAYS_IN_YEAR = 365;
+
+        // Input & Data Binding
+        System.out.println("Παρακαλώ εισάγετε την ηλικία σας σε έτη");
+        inputAgeInYears = scanner.nextInt();
+
+        // Λογική της εφαρμογής
+        ageInDays = inputAgeInYears * DAYS_IN_YEAR;
+
+        // Εκτύπωση αποτελεσμάτων
+        System.out.printf("Ηλικία σε έτη: %d, Ηλικία σε ημέρες: %d\n", inputAgeInYears, ageInDays);
+    }
+}
