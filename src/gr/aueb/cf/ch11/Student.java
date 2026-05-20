63aa58ed662664aa0ec7b093c247dfdf2a79467c
a8anassis <a8anassis@gmail.com>
2026-05-20 19:21:36 +0300

    chore(Student): encapsulate fields and add getter/setter methods

--- src/gr/aueb/cf/ch11/Student.java
+++ src/gr/aueb/cf/ch11/Student.java
@@ -5,7 +5,31 @@ package gr.aueb.cf.ch11;
  * Απλών και σύνθετων τύπων.
  */
 public class Student {
-    int id;
-    String firstname;
-    String lastname;
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
