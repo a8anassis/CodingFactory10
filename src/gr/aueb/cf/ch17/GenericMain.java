84a994164bfbb6fee36f76e1af37c2b931414c1e
a8anassis <a8anassis@gmail.com>
2026-06-04 20:35:35 +0300

    chore(GenericMain): uncomment and update usage of GenericNode with Integer type

--- src/gr/aueb/cf/ch17/GenericMain.java
+++ src/gr/aueb/cf/ch17/GenericMain.java
@@ -5,7 +5,7 @@ public class GenericMain {
     public static void main(String[] args) {
         GenericNode<String> strNode = new GenericNode<>();
         GenericNode<Double> doubleNode = new GenericNode<>();
-//        GenericNode<Integer> intNode = new GenericNode<Integer>();
+//        GenericNode<Integer> intNode = new GenericNode<>();
         var intNode = new GenericNode<Integer>();
 
         strNode.setValue("Hello");
