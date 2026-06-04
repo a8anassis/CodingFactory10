c9532422d636eb78d1a6c613fbe7536451b5b86d
a8anassis <a8anassis@gmail.com>
2026-06-04 20:36:13 +0300

    chore(Main): remove redundant blank line in main method

--- src/gr/aueb/cf/ch17/Main.java
+++ src/gr/aueb/cf/ch17/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         FlexibleNode flexibleNode = new FlexibleNode("Coding");
-
         int value = (int) flexibleNode.getValue();
 
         System.out.println(value);
