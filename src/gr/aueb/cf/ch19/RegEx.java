e472c303b7a548a7403cff9b5d59ec0211f2631d
a8anassis <a8anassis@gmail.com>
2026-06-29 19:40:54 +0300

    feat(app): add advanced RegEx methods for pattern matching and reverse string function

--- src/gr/aueb/cf/ch19/RegEx.java
+++ src/gr/aueb/cf/ch19/RegEx.java
@@ -3,9 +3,12 @@ package gr.aueb.cf.ch19;
 public class RegEx {
 
     public static void main(String[] args) {
-        String s = "Hi";
-        System.out.println(isHello(s));
-        System.out.println(isHelloOrHi(s));
+        String s = "hello coding";
+//        System.out.println(isHello(s));
+//        System.out.println(isHelloOrHi(s));
+
+//        System.out.println(startsWithLetter(s));
+        System.out.println(reverse(s));
     }
 
     // Απλά RegEx
@@ -29,4 +32,35 @@ public class RegEx {
     public static boolean startsWithLetter(String s) {
         return s.matches("[a-zA-Z0-9_]coding");
     }
+
+    // Μεταχαρακτήρες
+    public static boolean anyCharAndDigit(String s) {
+        return s.matches(".\\d");   // ".[0-9]"
+    }
+
+    public static boolean startsWithDotAndDigit(String s) {
+        return s.matches("\\.\\d"); // .2
+    }
+
+    // 02/04/2026 - ποσοδείκτες
+    public static boolean isDate(String s) {
+        return s.matches("\\d{2}/\\d{2}/\\d{4}");
+    }
+
+    public static boolean anyCharOneOrMore(String s) {
+        return s.matches(".+");
+    }
+
+    public static boolean anyCharZeroOrMore(String s) {
+        return s.matches(".*");
+    }
+
+    public static boolean twoWordsSpByOneSpace(String s) {
+        return s.matches(".+\\s.+");
+    }
+
+    public static String reverse(String s) {
+        return s.replaceAll("(.+)\\s(.+)", "$2 $1");    // groups - backreference
+    }
+
 }
